71fe3b90357d6504836058da2bee89f219fb529a
buzzcast0007-ui <buzzcast0007@gmail.com>
2026-02-20 17:43:56 +0000

    Create tailwind.config.js file

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: [
+    "./client/src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#667eea',
+        secondary: '#764ba2',
+      },
+      fontFamily: {
+        sans: ['Segoe UI', 'Roboto', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+  darkMode: 'class',
+};
